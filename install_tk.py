c0aed9c84ebb17c03337eb2b2b7b1f7fdbcf2a37
arvinzhu <arvinzhujun@gmail.com>
2019-08-23 09:18:55 +0800

    install_tk

---
+++ install_tk.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import os
+from xml.dom.minidom from parse
+tomcat_path = '/home/ctgtk/software/tomcat'
+user_path = 'tomcat_users.xml';
+userDom = parse(tomcat_path+'/conf/'+user_path)
+
+
